5d0c1e3f838f202eb983c4ca56b6c88b41800201
nakanori0508 <dd7bt8@bma.biglobe.ne.jp>
2021-03-01 22:42:03 +0900

    修正1

--- app/controllers/uipaths_controller.rb
+++ app/controllers/uipaths_controller.rb
@@ -22,14 +22,14 @@ class UipathsController < ApplicationController
 
   private
   def item_params
-    params.require(:ror).permit(:gem_name, :about_gem,:syntax_name,:version,:how_to_use,:description,:reference_url).merge(language_id: 1)
+    params.require(:uipath).permit(:package_name, :activity_name,:variable_type,:version,:how_to_use,:description,:reference_url).merge(language_id: 3)
   end
 
   def find_id
-    @rors = Language.find(1).rors
+    @uipaths = Language.find(3).uipaths
   end
 
   def get_arrays
-    @columns = ["gemファイル名","gemfileに記載する内容","構文名","バージョン","使い方","記載場所","参考URL"]
+    @columns = ["package名","Activity名","変数名","バージョン","使い方","記載場所","参考URL"]
   end
 end
